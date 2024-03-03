ca27664caa86e459967f17f63e640b2f8933e99d
Robert R <rarDevelopment@users.noreply.github.com>
2024-03-02 20:02:59 -0500

    switching to font awesome icons for navigation bar buttons

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,15 +1,12 @@
 import { Component } from '@angular/core';
-import { environment } from 'src/environments/environment';
 import { NgIf } from '@angular/common';
-import { NavbarButtonComponent } from '../shared/navbar-button/navbar-button.component';
+import { NavbarButtonComponent } from './navbar-button/navbar-button.component';
 
 @Component({
-    selector: 'app-navbar',
-    templateUrl: './navbar.component.html',
-    styleUrls: ['./navbar.component.scss'],
-    standalone: true,
-    imports: [NavbarButtonComponent, NgIf],
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.scss'],
+  standalone: true,
+  imports: [NavbarButtonComponent, NgIf],
 })
-export class NavbarComponent {
-  public isSocialEnabled: boolean = environment.isSocialEnabled;
-}
+export class NavbarComponent {}
