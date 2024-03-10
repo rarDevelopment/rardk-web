da8d4d1a660f8334548f2c8edc63595ecbc49f56
Robert R <rarDevelopment@users.noreply.github.com>
2024-03-10 01:05:32 -0500

    add page title import

--- src/app/components/pixelfed/pixelfed.component.ts
+++ src/app/components/pixelfed/pixelfed.component.ts
@@ -3,11 +3,12 @@ import { PixelfedPost } from 'src/app/models/pixelfed/pixelfed-post';
 import { PixelfedService } from './pixelfed.service';
 import { take } from 'rxjs';
 import { NgFor } from '@angular/common';
+import { PageTitleComponent } from '../shared/page-title/page-title.component';
 
 @Component({
   selector: 'app-pixelfed',
   standalone: true,
-  imports: [NgFor],
+  imports: [NgFor, PageTitleComponent],
   templateUrl: './pixelfed.component.html',
   styleUrl: './pixelfed.component.scss',
 })
