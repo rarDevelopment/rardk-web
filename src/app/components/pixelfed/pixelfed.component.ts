5c80de09c3b50374f02be35c8cc6fae6171574d9
Robert R <rarDevelopment@users.noreply.github.com>
2024-03-10 22:01:41 -0400

    gallery grid and modal improvements

--- src/app/components/pixelfed/pixelfed.component.ts
+++ src/app/components/pixelfed/pixelfed.component.ts
@@ -6,11 +6,19 @@ import { NgFor, NgIf } from '@angular/common';
 import { PageTitleComponent } from '../shared/page-title/page-title.component';
 import { ModalComponent } from '../shared/modal/modal.component';
 import { LoadingIndicatorComponent } from '../shared/loading-indicator/loading-indicator.component';
+import { DateDisplayComponent } from '../shared/date-display/date-display.component';
 
 @Component({
   selector: 'app-pixelfed',
   standalone: true,
-  imports: [NgFor, NgIf, PageTitleComponent, ModalComponent, LoadingIndicatorComponent],
+  imports: [
+    NgFor,
+    NgIf,
+    DateDisplayComponent,
+    PageTitleComponent,
+    ModalComponent,
+    LoadingIndicatorComponent,
+  ],
   templateUrl: './pixelfed.component.html',
   styleUrl: './pixelfed.component.scss',
 })
