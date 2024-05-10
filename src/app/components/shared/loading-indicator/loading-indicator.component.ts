a96c7fade6b29eb95180cf6ca3af6ba369f351fe
Robert R <rarDevelopment@users.noreply.github.com>
2024-05-09 22:13:56 -0400

    tidy up loading component usage

--- src/app/components/shared/loading-indicator/loading-indicator.component.ts
+++ src/app/components/shared/loading-indicator/loading-indicator.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
+import { NgIf } from '@angular/common';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-loading-indicator',
   standalone: true,
-  imports: [],
+  imports: [NgIf],
   templateUrl: './loading-indicator.component.html',
   styleUrl: './loading-indicator.component.scss',
 })
-export class LoadingIndicatorComponent {}
+export class LoadingIndicatorComponent {
+  @Input() isLoading: boolean;
+}
