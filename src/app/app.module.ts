89df4e38bc6470f56ad7bb2ea128d762cbdbe532
Robert R <rarDevelopment@users.noreply.github.com>
2024-05-26 23:38:53 -0400

    Revert "Revert "upgrade angular version""
    This reverts commit 911cb66fdf452306d8d55a3632a617688daa9c51.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,38 +3,32 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HttpClientModule, provideHttpClient, withFetch } from '@angular/common/http';
+import { provideHttpClient, withFetch, provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { MAT_DIALOG_DEFAULT_OPTIONS } from '@angular/material/dialog';
 import { MarkdownModule } from 'ngx-markdown';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { TooltipDirective } from './directives/tooltip.directive';
 
-@NgModule({
-  declarations: [AppComponent],
-  imports: [
-    BrowserModule,
-    BrowserAnimationsModule,
-    AppRoutingModule,
-    NavbarComponent,
-    HttpClientModule,
-    TooltipDirective,
-    MarkdownModule.forRoot(),
-    ServiceWorkerModule.register('ngsw-worker.js', {
-      enabled: !isDevMode(),
-      // Register the ServiceWorker as soon as the application is stable
-      // or after 30 seconds (whichever comes first).
-      registrationStrategy: 'registerWhenStable:30000'
-    }),
-  ],
-  providers: [
-    {
-      provide: MAT_DIALOG_DEFAULT_OPTIONS,
-      useValue: { hasBackdrop: true, disableClose: true },
-    },
-    provideClientHydration(),
-    provideHttpClient(withFetch()),
-  ],
-  bootstrap: [AppComponent],
-})
+@NgModule({ declarations: [AppComponent],
+    bootstrap: [AppComponent], imports: [BrowserModule,
+        BrowserAnimationsModule,
+        AppRoutingModule,
+        NavbarComponent,
+        TooltipDirective,
+        MarkdownModule.forRoot(),
+        ServiceWorkerModule.register('ngsw-worker.js', {
+            enabled: !isDevMode(),
+            // Register the ServiceWorker as soon as the application is stable
+            // or after 30 seconds (whichever comes first).
+            registrationStrategy: 'registerWhenStable:30000'
+        })], providers: [
+        {
+            provide: MAT_DIALOG_DEFAULT_OPTIONS,
+            useValue: { hasBackdrop: true, disableClose: true },
+        },
+        provideClientHydration(),
+        provideHttpClient(withFetch()),
+        provideHttpClient(withInterceptorsFromDi()),
+    ] })
 export class AppModule {}
