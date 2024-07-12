4b4aae4365aef2912955cb85e0f294ec6b7842c2
Robert R <rarDevelopment@users.noreply.github.com>
2024-07-12 01:05:43 -0400

    add posts to home page, adjust styles

--- src/app/components/posts/posts.component.ts
+++ src/app/components/posts/posts.component.ts
@@ -1,5 +1,5 @@
-import { NgFor, NgIf } from '@angular/common';
-import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Input, OnInit } from '@angular/core';
 import { HtmlDirective } from 'src/app/directives/html.directive';
 import { PageTitleComponent } from '../shared/page-title/page-title.component';
 import { finalize, take } from 'rxjs';
@@ -7,17 +7,18 @@ import { DateDisplayComponent } from '../shared/date-display/date-display.compon
 import { LoadingIndicatorComponent } from '../shared/loading-indicator/loading-indicator.component';
 import { PostsService } from './posts.service';
 import { Post } from './models/post';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-posts',
   standalone: true,
   imports: [
-    NgFor,
+    CommonModule,
     HtmlDirective,
     PageTitleComponent,
-    NgIf,
     LoadingIndicatorComponent,
     DateDisplayComponent,
+    RouterLink,
   ],
   templateUrl: './posts.component.html',
   styleUrl: './posts.component.scss',
@@ -26,6 +27,9 @@ export class PostsComponent implements OnInit {
   constructor(private postsService: PostsService) {}
   public posts: Post[];
   public isLoading: boolean;
+  @Input() itemCount: number = 0;
+  @Input() pageTitle = 'Posts';
+  @Input() showPageLink = false;
 
   async ngOnInit() {
     this.populatePosts();
@@ -43,7 +47,12 @@ export class PostsComponent implements OnInit {
       )
       .subscribe({
         next: (postsResponse) => {
-          this.posts = postsResponse.sort((l1, l2) => (l1.posted_at > l2.posted_at ? -1 : 1));
+          const postsToShow = postsResponse.sort((l1, l2) =>
+            l1.posted_at > l2.posted_at ? -1 : 1
+          );
+          this.itemCount > 0
+            ? (this.posts = postsToShow.slice(0, this.itemCount))
+            : (this.posts = postsToShow);
         },
         error: (error) => {
           console.error(error);
