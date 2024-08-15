22a66050f677fd3886bc55eb5550c51c816b28a0
Robert R <rarDevelopment@users.noreply.github.com>
2024-08-15 00:18:57 -0400

    made navbar collapsible and collapsed by default on mobile

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,5 +1,7 @@
+import { CommonModule } from '@angular/common';
+import { animate, state, style, transition, trigger } from '@angular/animations';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { Component } from '@angular/core';
-
 import { NavbarButtonComponent } from './navbar-button/navbar-button.component';
 
 @Component({
@@ -7,6 +9,12 @@ import { NavbarButtonComponent } from './navbar-button/navbar-button.component';
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss'],
   standalone: true,
-  imports: [NavbarButtonComponent],
+  imports: [NavbarButtonComponent, CommonModule],
 })
-export class NavbarComponent {}
+export class NavbarComponent {
+  public showNavbarOnMobile = false;
+
+  toggleNavbar() {
+    this.showNavbarOnMobile = !this.showNavbarOnMobile;
+  }
+}
