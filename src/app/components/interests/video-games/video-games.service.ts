3c3998b839887ee1799719a6954fd4ff13fa9119
Robert R <rarDevelopment@users.noreply.github.com>
2024-09-11 23:29:02 -0400

    decode encoded characters in game values

--- src/app/components/interests/video-games/video-games.service.ts
+++ src/app/components/interests/video-games/video-games.service.ts
@@ -1,21 +1,32 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { map, Observable } from 'rxjs';
 import { GameCollectionEntry } from 'src/app/components/interests/video-games/models/game-collection-entry';
 import { environment } from 'src/environments/environment';
 import { FavouriteGame } from './models/favourite-game';
+import { HtmlDecoder } from 'src/app/utilities/html-decoder';
 
 @Injectable({
   providedIn: 'root',
 })
 export class VideoGamesService {
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private htmlDecoder: HtmlDecoder) {}
 
   public getGameCollection(): Observable<GameCollectionEntry[]> {
     return this.http.get<GameCollectionEntry[]>(`${environment.apiUrl}now/games/collection`);
   }
 
   getFavouriteGames(): Observable<FavouriteGame[]> {
-    return this.http.get<FavouriteGame[]>(`${environment.feedsSiteUrl}rss/favourite-games/json/`);
+    return this.http
+      .get<FavouriteGame[]>(`${environment.feedsSiteUrl}rss/favourite-games/json/`)
+      .pipe(
+        map((games: FavouriteGame[]) =>
+          games.map((g) => ({
+            ...g,
+            gameName: this.htmlDecoder.htmlEntityDecode(g.gameName),
+            imageId: this.htmlDecoder.htmlEntityDecode(g.imageId),
+          }))
+        )
+      );
   }
 }
