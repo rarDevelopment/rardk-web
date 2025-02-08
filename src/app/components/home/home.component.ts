29eb5dc7a45b967b6f869eea0e2171f925fadc51
Robert R <rarDevelopment@users.noreply.github.com>
2025-02-08 01:26:41 -0500

    birthday changes 2025

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,13 +1,9 @@
 import { Component } from '@angular/core';
-import { BlogComponent } from '../blog/blog.component';
-import { GalleryComponent } from '../gallery/gallery.component';
-import { PostsComponent } from '../posts/posts.component';
-import { LinksComponent } from '../links/links.component';
 
 @Component({
     selector: 'app-home',
     templateUrl: './home.component.html',
     styleUrl: './home.component.scss',
-    imports: [BlogComponent, GalleryComponent, PostsComponent, LinksComponent]
+    imports: []
 })
 export class HomeComponent {}
