5d7c1198cd897c67f3b1bf59e21edf44fb486d16
Robert <4060573+rarDevelopment@users.noreply.github.com>
2025-02-23 01:25:35 -0500

    Revert "birthday changes 2025"

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
+import { BlogComponent } from '../blog/blog.component';
+import { GalleryComponent } from '../gallery/gallery.component';
+import { PostsComponent } from '../posts/posts.component';
+import { LinksComponent } from '../links/links.component';
+import { MediaFeedComponent } from "../now/media-feed/media-feed.component";
 
 @Component({
     selector: 'app-home',
     templateUrl: './home.component.html',
     styleUrl: './home.component.scss',
-    imports: []
-
+    imports: [BlogComponent, GalleryComponent, PostsComponent, LinksComponent, MediaFeedComponent]
 })
 export class HomeComponent {}
